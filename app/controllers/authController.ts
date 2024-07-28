90d3f3b5353eae3010b8c58951cebc0be7aa290b
Adrian Garcia <adrianisa123575@outlook.com>
2024-07-28 15:31:03 -0500

    feat: :sparkles: implementar la autenticación de usuarios

---
+++ app/controllers/authController.ts
@@ -0,0 +1,51 @@
+import bcryptjs from 'bcryptjs';
+import { Request, Response } from 'express';
+
+import User from '../models/userModel'
+import { generateJWT } from '../helpers/jwtUtils';
+
+import { Login } from '../types/Login';
+
+const login = async ( req: Request<{}, {}, Login>, res: Response ) => {
+
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ where: {email} });
+
+        if ( !user ) {
+            return res.json({
+                msg: 'El correo o contraseña es incorrecta.'
+            });
+        }
+
+        const validatePassword: boolean = bcryptjs.compareSync( password, user.password );
+        if( !validatePassword ) {
+            return res.json({
+                msg: 'La contraseña es incorrecta.'
+            });
+        }
+
+        if ( !user.state ) {
+            return res.json({
+               msg: 'La cuenta esta desactivada.' 
+            });
+        }
+
+        const token: string = await generateJWT( user.id );
+
+        res.json({
+            user,
+            token
+        });
+    } catch ( error ) {
+        console.log( error );
+        res.status(500).json({
+            msg: 'Contacte con el administrador.'
+        });
+    }
+}
+
+
+
+export { login };
