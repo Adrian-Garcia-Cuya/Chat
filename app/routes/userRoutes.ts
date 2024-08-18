589bfe0e6f4b99e38158236f7a4feeee4b0b59d7
Adrian Garcia <adrianisa123575@outlook.com>
2024-08-18 18:48:19 -0500

    refactor: :recycle: cambiar el nombre de la propiedad que se valida
    Actualización de la propiedad `phone` a `phoneNumber` en la validación.

--- app/routes/userRoutes.ts
+++ app/routes/userRoutes.ts
@@ -13,7 +13,7 @@ router.post( '/', [
     body('email', 'El email no es valido.').notEmpty().isEmail().isString().trim(),
     body('email', ).custom( checkEmailNotInUse ),
     body('password', 'La contraseña no puede estar vacia y debe ser mayor a 8 caracteres.').notEmpty().isLength({ min: 8 }),
-    body('phone', 'El numero ingresado no debe ser mayor ni menor a 9 digitos.').notEmpty().isInt().isLength({ min: 9, max: 9}),
+    body('phoneNumber', 'El numero ingresado no debe ser mayor ni menor a 9 digitos.').notEmpty().isInt().isLength({ min: 9, max: 9}),
     body('image_url', 'Debe ingresar una URL valida.').optional().isString(),
     body('state').isBoolean().optional(),
     checkValidationErrors
@@ -23,7 +23,7 @@ router.put( '/:id', [
     param('id', 'El id debe ser un entero.').isInt(),
     param('id').custom( checkUserExistence ),
     body('name', 'Es necesario agregar un nombre.').notEmpty().optional(),
-    body('phone', 'El numero ingresado no debe ser mayor ni menor a 9 digitos.').notEmpty().isInt().isLength({ min: 9, max: 9}).optional(),
+    body('phoneNumber', 'El numero ingresado no debe ser mayor ni menor a 9 digitos.').notEmpty().isInt().isLength({ min: 9, max: 9}).optional(),
     body('image_url', 'Debe ingresar una URL valida.').optional().isString(),
     checkJWT,
     checkValidationErrors
