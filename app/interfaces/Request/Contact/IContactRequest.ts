4737d031b4437ce6467d186892204dde124546b0
Adrian Garcia <adrianisa123575@outlook.com>
2024-08-18 18:28:57 -0500

    feat: :sparkles: añadir nueva interfaz para los contactos

---
+++ app/interfaces/Request/Contact/IContactRequest.ts
@@ -0,0 +1,12 @@
+import { ContactPhoneNumberParams } from "../../../types/Contact/ContactParams";
+import { ContactBody } from "../../../types/Contact/ContactRequest";
+import { BaseRequest } from "../BaseRequest";
+
+//NOTE: Estas interfaces se utilizan pero no necesariamente se requieren asi, ahora. Está, mas que todo, de ejemplo en caso se requira de esta forma mas adelante.
+export interface CreateContactRequest extends BaseRequest<{}, {}, ContactBody, {}> {
+
+}
+
+export interface DeleteContactRequest extends BaseRequest<ContactPhoneNumberParams, {}, ContactBody, {}> {
+
+}
