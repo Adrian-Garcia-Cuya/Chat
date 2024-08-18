48e645fbff28fdc114836737ea0573cffd0d631f
Adrian Garcia <adrianisa123575@outlook.com>
2024-08-18 18:42:34 -0500

    refactor: :recycle: actualizar el flujo de la función

--- app/middlewares/validationHandler.ts
+++ app/middlewares/validationHandler.ts
@@ -6,9 +6,10 @@ const checkValidationErrors = ( req: Request, res: Response, next: NextFunction 
     const errors = validationResult( req );
     if ( !errors.isEmpty() ) {
         res.status(400).json({ errors: errors.array() });
-    } else {
-        next();
+        return;
     }
+    
+    next();
 }
 
 export {
