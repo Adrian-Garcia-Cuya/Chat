80e9df3531fb9e1f45855e4f0a6f53401a908578
Adrian Garcia <adrianisa123575@outlook.com>
2024-09-07 19:23:59 -0500

    feat: :sparkles: implementar la unión a salas y comunicación entre clientes

--- sockets/socketHandler.ts
+++ sockets/socketHandler.ts
@@ -1,9 +1,30 @@
 import { Socket } from "socket.io";
+import { verifyJWT } from "../app/helpers/jwtUtils";
+import { isString } from "../app/helpers/predicates";
+import { IMessageInformation } from "../app/interfaces/Socket/IMessageInformation";
 
-const socketHandler = ( socket: Socket ) => {
+const socketHandler = async( socket: Socket ) => {
 
     console.log( 'Conexion establecida.' );
+    const token = socket.handshake.headers['x-token'];
+    if ( !isString( token ) ) {
+        console.error('Token invalido o ausente.')
+        return socket.disconnect();
+    }
+    const user = await verifyJWT( JSON.parse(token) );
+    if ( !user ) {
+        return socket.disconnect();
+    }
+
+    socket.join(String(user.id));
+
+    socket.on('send-message', ( { userId, message }: IMessageInformation ): void => {
+        socket.to( String( userId ) ).emit( 'private-message', { by: user.name, message } );
+    });
+
 }
+
+
 
 export {
     socketHandler
