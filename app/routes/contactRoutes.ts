21659ffbc5df04f913f8c002d806e9de1a52d442
Adrian Garcia <adrianisa123575@outlook.com>
2024-09-07 19:22:08 -0500

    chore: agregar validación de token al obtener los contactos de un usuario

--- app/routes/contactRoutes.ts
+++ app/routes/contactRoutes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { body, param } from "express-validator";
 
-import { destroy, store } from "../controllers/contactController";
+import { destroy, index, store } from "../controllers/contactController";
 import { checkJWT } from "../middlewares/jwtValidation";
 import { checkContactExistence, checkContactIsFriend, checkValidationErrors } from "../middlewares/validationHandler";
 
 const router = Router();
+
+router.get( '/', [
+    checkJWT
+], index);
 
 router.post( '/', [
     checkJWT,
