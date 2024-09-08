60353fd8a537ed24d31e753768ce3ed523ed5345
Adrian Garcia <adrianisa123575@outlook.com>
2024-09-07 19:11:55 -0500

    feat: :safety_vest: verificar si el token de autenticación está presente en la conexión de socket

--- app/helpers/jwtUtils.ts
+++ app/helpers/jwtUtils.ts
@@ -1,5 +1,7 @@
-import jwt, { Secret } from 'jsonwebtoken'
+import jwt, { Secret, verify } from 'jsonwebtoken'
 import { Payload } from '../types/auth/Payload';
+import User from '../models/userModel';
+import { isPayload } from './predicates';
 
 const generateJWT = ( userId: string ): Promise<string> => {
     const secretKey: Secret|undefined =  process.env.SECRET_KEY;
@@ -26,6 +28,41 @@ const generateJWT = ( userId: string ): Promise<string> => {
     });
 }
 
+const verifyJWT = async( token: string ): Promise<User|undefined> => {
+
+    const secretKey: Secret|undefined =  process.env.SECRET_KEY;
+    try{
+        if ( !secretKey ) {
+            throw new Error( 'La llave secreta no está definida.' );
+        }
+
+        if ( token.length < 10 ) {
+            throw new Error( 'Token invalido.' );
+        }
+
+        const payload = jwt.verify( token, secretKey );
+
+        if ( !isPayload( payload ) ) {
+            throw new Error( 'No se encontró el id del usuario en el payload.' );
+        }
+
+        const userId = payload.userId;
+        const user = await User.findByPk( userId );
+        if ( !user ) {
+            throw new Error( 'Token invalido/ Usuario no existe.' );
+        }
+        if ( !user.state ) {
+            throw new Error( 'Esta cuenta esta desactivada.' );
+        }
+
+        return user;
+
+    } catch ( error: any ) {
+        console.log(error);
+    }
+}
+
 export {
-    generateJWT
+    generateJWT,
+    verifyJWT
 };
