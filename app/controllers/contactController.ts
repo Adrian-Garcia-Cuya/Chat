a86e9cda40d63da2741906e4168fd40d9d255f95
Adrian Garcia <adrianisa123575@outlook.com>
2024-09-07 19:01:48 -0500

    feat: :sparkles: agregar método para obtener todos los contactos de un usuario

--- app/controllers/contactController.ts
+++ app/controllers/contactController.ts
@@ -1,6 +1,52 @@
 import { Response } from "express";
 import Friend from "../models/friendModel";
 import { ContactRequest } from "../interfaces/Request/Contact/IContactRequest";
+import FriendConfiguration from "../models/friendConfigurationModel";
+import User from "../models/userModel";
+
+const index = async (req: ContactRequest, res: Response): Promise<void> => {
+    try {
+        const { user } = req;
+
+        if ( !user ) {
+            throw new Error( 'Acceso denegado.' );
+        }
+
+        const friends: Friend[] = await Friend.findAll({
+            where: {
+                user_id: user.id
+            }
+        });
+
+        if ( !friends ) {
+            res.status(404).json({ 
+                msg: 'No hay amigos registrados'
+            });
+            return;
+        }
+
+        const listFriends = await Promise.all(
+            friends.map( async ( friend ) => {
+                const friendConfiguration = await FriendConfiguration.findByPk( friend.id );
+                const user = await User.findByPk( friend.friend_id, {
+                    attributes: ['id', 'name', 'phoneNumber', 'image_url']
+                } );
+                return { friend, user, friendConfiguration }
+                
+            })
+            
+        );
+        
+
+        res.status(200).json({ listFriends });
+        return;
+    } catch (error: any) {
+        console.error(error.message);
+        res.status(500).json({ 
+            msg: 'Internal Server Error'
+        });
+    }
+}
 
 const store = async ( req: ContactRequest, res: Response): Promise<void> => {
 
@@ -13,12 +59,12 @@ const store = async ( req: ContactRequest, res: Response): Promise<void> => {
         });
 
         res.status(200).json({
-            message: 'Registro exitoso.',
+            msg: 'Registro exitoso.',
         });
         return;
     } catch( error: any ) {
         res.status(400).json({
-            message: error.message
+            msg: error.message
         });
         return;
     }
@@ -26,8 +72,8 @@ const store = async ( req: ContactRequest, res: Response): Promise<void> => {
 
 const destroy = async( req: ContactRequest, res: Response ): Promise<void> => {
     
-    const friendId = req.friendId!;
-    const userId = req.id!;
+    const friendId = req.friendId;
+    const userId = req.id;
 
     try {
         const [ affectedRows ] = await Friend.update(
@@ -52,13 +98,14 @@ const destroy = async( req: ContactRequest, res: Response ): Promise<void> => {
     } catch ( error: any ) {
         console.error( error );
         res.status(400).json({
-            message: error.message
+            msg: error.message
         });
         return;
     }
 }
 
 export {
+    index,
     store,
     destroy
 }
